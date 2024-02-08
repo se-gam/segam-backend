08dd3e1bdc88a5a59eafecf41cb18f8fd67bd04e
James Jung <therealjamesjung@gmail.com>
2024-02-08 18:04:46 +0900

    feat: ConfigModule 설정

---
+++ src/app/modules/config.module.ts
@@ -0,0 +1,12 @@
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
+
+export const configModule = ConfigModule.forRoot({
+  isGlobal: true,
+  envFilePath: `.${process.env.NODE_ENV}.env`,
+  validationSchema: Joi.object({
+    NODE_ENV: Joi.string().valid('dev', 'prod', 'local'),
+    AWS_ACCESS_KEY: Joi.string().required(),
+    AWS_SECRET_KEY: Joi.string().required(),
+  }),
+});
