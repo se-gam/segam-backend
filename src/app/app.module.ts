b8e9af21b2d10fcb26bba331fa44f6f780ac05e4
James Jung <therealjamesjung@gmail.com>
2024-02-08 18:20:03 +0900

    feat: Studyroom 모듈 init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { CommonModule } from 'src/common/common.module';
 import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+import { StudyroomModule } from 'src/studyroom/studyroom.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [CommonModule],
+  imports: [CommonModule, StudyroomModule],
   controllers: [AppController],
   providers: [AppService],
 })
