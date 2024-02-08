ba3566794354f8a8b09d696da8cba409c277a78e
James Jung <therealjamesjung@gmail.com>
2024-02-08 17:56:13 +0900

    feat: ValidationPipe init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      stopAtFirstError: true,
+    }),
+  );
+
+  app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
