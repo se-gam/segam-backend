5bd99d124bb09fe8eeeb15e7d19ffe315d110960
James Jung <therealjamesjung@gmail.com>
2024-02-10 16:15:51 +0900

    feat: swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 import { HttpExceptionFilter } from './common/exception/exception.filter';
 
@@ -18,10 +19,23 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
 
+  const config = new DocumentBuilder()
+    .setTitle('Segam Backend API')
+    .setDescription('세감 백엔드 API 문서입니다.')
+    .setVersion('1.0')
+    .addTag('Segam')
+    // .addBearerAuth()
+    .build();
+
   app.enableVersioning({
     type: VersioningType.URI,
     prefix: 'v',
   });
+
+  const document = SwaggerModule.createDocument(app, config);
+  if (process.env.NODE_ENV !== 'prod') {
+    SwaggerModule.setup('docs', app, document);
+  }
 
   await app.listen(3000);
 }
