d9ebe240d8d03246e3d0c24895fe6f6b5454efbe
James Jung <therealjamesjung@gmail.com>
2024-02-15 01:05:28 +0900

    feat: Swagger 인증 방식 추가 및 health 체크 API 추가

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
+import { JwtAuthGuard } from 'src/auth/guard/jwt-auth.guard';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +8,16 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Health Check' })
   getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('private')
+  @UseGuards(JwtAuthGuard)
+  @ApiOperation({ summary: 'Private Health Check' })
+  @ApiBearerAuth()
+  getHelloPrivate(): string {
     return this.appService.getHello();
   }
 }
