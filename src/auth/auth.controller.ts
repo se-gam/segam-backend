0dfdabbecfce23fc17c9e4551157ff88116fdd07
James Jung <therealjamesjung@gmail.com>
2024-02-15 01:14:13 +0900

    feat: 토큰 refresh API

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Post, Version } from '@nestjs/common';
 import { ApiCreatedResponse, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { TokenDto } from './dto/token.dto';
+import { RefreshTokenPayload } from './payload/refresh-token.payload';
 import { SignUpPayload } from './payload/signup.payload';
 
 @Controller('auth')
@@ -18,5 +19,16 @@ export class AuthController {
   @ApiCreatedResponse({ type: TokenDto })
   async signup(@Body() payload: SignUpPayload): Promise<TokenDto> {
     return this.authService.signup(payload);
+  }
+
+  @Version('1')
+  @Post('refresh')
+  @ApiOperation({
+    summary: '토큰 갱신 API',
+    description: '기존 토큰을 갱신합니다.',
+  })
+  @ApiCreatedResponse({ type: TokenDto })
+  async refresh(@Body() payload: RefreshTokenPayload): Promise<TokenDto> {
+    return this.authService.refresh(payload.refreshToken);
   }
 }
