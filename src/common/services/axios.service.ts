53eb3d81d1db4dff1122e79dfa49cb3dc77afed8
James Jung <therealjamesjung@gmail.com>
2024-02-15 01:01:43 +0900

    feat: Axios 모듈 의존성으로 분리

---
+++ src/common/services/axios.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@nestjs/common';
+import { Axios } from 'axios';
+import { constants } from 'crypto';
+import { HttpsCookieAgent } from 'http-cookie-agent/http';
+import { CookieJar } from 'tough-cookie';
+
+@Injectable()
+export class AxiosService extends Axios {
+  constructor() {
+    const jar = new CookieJar();
+
+    const httpsAgent = new HttpsCookieAgent({
+      cookies: { jar },
+      secureOptions: constants.SSL_OP_LEGACY_SERVER_CONNECT,
+    });
+
+    super({
+      withCredentials: true,
+      httpsAgent,
+    });
+  }
+}
