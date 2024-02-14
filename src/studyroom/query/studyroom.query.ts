2feab4618ae7da1b3dbc72c6170d2172fb3b8fef
Yerin <myheaven_b@naver.com>
2024-02-14 14:31:52 +0900

    refactor: query 인자를 optional로 변경 및 default 제공

--- src/studyroom/query/studyroom.query.ts
+++ src/studyroom/query/studyroom.query.ts
@@ -1,24 +1,22 @@
+import { Optional } from '@nestjs/common';
 import { Type } from 'class-transformer';
-import { IsDate, IsDateString, IsInt, IsString, isInt } from 'class-validator';
+import { IsDate, IsInt } from 'class-validator';
 
 export class StudyroomQuery {
   @Type(() => Date)
   @IsDate()
-  date!: Date;
+  @Optional()
+  date: Date = new Date();
 
   @Type(() => Number)
   @IsInt()
-  timeGte!: number;
-  /*
-  TODO
-  이런 필터 옵션들은 Optional로 받는 것이 좋습니당
-  값이 안들어왔을 때는 디폴트로 예를들어 여기에는 오늘날짜, 모든 인원수로 보여지게 하는 식으로 설계하는 것이 조와요
-  */
-  @Type(() => Number)
-  @IsInt()
-  timeLte!: number;
+  timeGte: number = 10;
 
   @Type(() => Number)
   @IsInt()
-  userCount!: number;
+  timeLte: number = 10;
+
+  @Type(() => Number)
+  @IsInt()
+  userCount: number = 3;
 }
