87500999d68657883894d910e0ca1da19595d5ee
James Jung <therealjamesjung@gmail.com>
2024-03-14 01:26:06 +0900

    feat:  출석정보 업데이트 API 요청수 제한

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AttendanceModule } from 'src/attendance/attendance.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
 import { LoggerMiddleware } from 'src/common/middlewares/logger.middleware';
+import { RestaurantModule } from 'src/restaurant/restaurant.module';
 import { StudyroomModule } from 'src/studyroom/studyroom.module';
 import { UserModule } from 'src/user/user.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { configModule } from './modules/config.module';
-import { RestaurantModule } from 'src/restaurant/restaurant.module';
 
 @Module({
   imports: [
@@ -19,9 +20,21 @@ import { RestaurantModule } from 'src/restaurant/restaurant.module';
     AttendanceModule,
     UserModule,
     RestaurantModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'APP_GUARD',
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
