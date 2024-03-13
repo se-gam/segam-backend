ae746219f287b20ed6c4d794c51768853a5d8757
James Jung <therealjamesjung@gmail.com>
2024-03-14 02:35:28 +0900

    remove: throttler 모듈 삭제

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
-import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AttendanceModule } from 'src/attendance/attendance.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { CommonModule } from 'src/common/common.module';
@@ -20,21 +19,9 @@ import { configModule } from './modules/config.module';
     AttendanceModule,
     UserModule,
     RestaurantModule,
-    ThrottlerModule.forRoot([
-      {
-        ttl: 60000,
-        limit: 10,
-      },
-    ]),
   ],
   controllers: [AppController],
-  providers: [
-    AppService,
-    {
-      provide: 'APP_GUARD',
-      useClass: ThrottlerGuard,
-    },
-  ],
+  providers: [AppService],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
