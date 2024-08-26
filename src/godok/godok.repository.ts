f25f971e70518af935d69aa4cfd415e65e878ebf
Yerin <myheaven_b@naver.com>
2024-08-26 23:20:33 +0900

    feat: 고전독서 예약현황 update

--- src/godok/godok.repository.ts
+++ src/godok/godok.repository.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { GodokSlot } from '@prisma/client';
+import * as _ from 'lodash';
 import { PrismaService } from 'src/common/services/prisma.service';
+import { GodokReservationResponse } from './types/godokReservationResponse.type';
 
 @Injectable()
 export class GodokRepository {
@@ -22,5 +24,68 @@ export class GodokRepository {
     });
 
     return slots;
+  }
+
+  async getBookIdsByNames(names: string[]): Promise<Map<string, number>> {
+    const books = await this.prismaService.book.findMany({
+      where: {
+        title: {
+          in: names,
+        },
+      },
+      select: {
+        id: true,
+        title: true,
+      },
+    });
+
+    return new Map(books.map((book) => [book.title, book.id]));
+  }
+
+  async updateUserReservation(
+    userId: string,
+    newReservations: GodokReservationResponse[],
+  ) {
+    // 이전 예약 id 탐색
+    const prevIds = await this.getReservationIds(userId);
+    const newIds = newReservations.map((reservation) => reservation.reserve_id);
+
+    const existingIds = _.intersection(prevIds, newIds);
+    const deletedIds = _.difference(prevIds, existingIds);
+    const createdReservations = newReservations.filter((reservation) =>
+      _.difference(newIds, existingIds).includes(reservation.reserve_id),
+    );
+
+    // 사라진 예약 삭제
+    await this.prismaService.godokReservation.updateMany({
+      where: {
+        reservationId: {
+          in: deletedIds,
+        },
+      },
+      data: {
+        deletedAt: new Date(),
+      },
+    });
+
+    // bookId 탐색
+    const bookNames = createdReservations.map(
+      (reservation) => reservation.book_name,
+    );
+    const bookData = await this.getBookIdsByNames(bookNames);
+
+    // 새로운 예약 생성
+    await this.prismaService.godokReservation.createMany({
+      data: [
+        ...createdReservations.map((reservation) => {
+          return {
+            reservationId: reservation.reserve_id,
+            studentId: userId,
+            bookId: bookData.get(reservation.book_name),
+            startsAt: new Date(reservation.date_time),
+          };
+        }),
+      ],
+    });
   }
 }
--- src/godok/godok.service.ts
+++ src/godok/godok.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { Cron } from '@nestjs/schedule';
 import { AxiosService } from 'src/common/services/axios.service';
@@ -6,11 +12,15 @@ import { PrismaService } from 'src/common/services/prisma.service';
 import { GodokSlotListDto } from './dto/godok-slot.dto';
 import { GodokRepository } from './godok.repository';
 import { RawGodokSlot } from './types/rawGodokSlot.type';
+import { GodokReservePayload } from './payload/godokReserve.payload';
+import { ResultResponse } from 'src/studyroom/types/resultResponse.type';
+import { UserRepository } from 'src/user/user.repository';
 
 @Injectable()
 export class GodokService {
   constructor(
     private readonly godokRepository: GodokRepository,
+    private readonly userRepository: UserRepository,
     private readonly axiosService: AxiosService,
     private readonly configService: ConfigService,
     private readonly prismaService: PrismaService,
@@ -42,6 +52,31 @@ export class GodokService {
         },
       });
     }
+  }
+
+  async updateUserGodokReservation(userId: string, password: string) {
+    const user = await this.userRepository.getUserByStudentId(userId);
+
+    if (!user)
+      throw new NotFoundException('해당 학번의 학생이 존재하지 않습니다');
+
+    const res = await this.axiosService.post(
+      this.configService.get<string>('GET_USER_GODOK_RESERVATIONS_URL'),
+      JSON.stringify({ student_id: userId, password: password }),
+      { headers: { 'Content-Type': 'application/json' } },
+    );
+
+    const response = JSON.parse(res.data);
+
+    if (res.status === 400) {
+      throw new BadRequestException(response.error);
+    } else if (res.status === 401) {
+      throw new UnauthorizedException(response.error);
+    } else if (res.status >= 400) {
+      throw new InternalServerErrorException('Internal Server Error');
+    }
+
+    await this.godokRepository.updateUserReservation(userId, response.result);
   }
 
   async getGodokCalendar(): Promise<GodokSlotListDto> {
