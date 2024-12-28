a52d7fdf2d3c79c0d05fd2df23836dedbd8a1884
James Jung <therealjamesjung@gmail.com>
2024-12-29 03:23:59 +0900

    fix: 학번, 이름 마스킹

--- src/auth/auth.repository.ts
+++ src/auth/auth.repository.ts
@@ -27,7 +27,7 @@ export class AuthRepository {
       },
     });
     this.discordService.sendNewUserLog(
-      `🎉*회원가입 알림*🎉\n${studentId} ${name}님이 ${rejoined ? '재' : ''}가입하셨습니다!!\n\n🔥전체 유저 수: ${number2emoji(userCount)}명 돌파!!🔥`,
+      `🎉*회원가입 알림*🎉\n${studentId.slice(0, 2)}****** ${name.slice(0, 1)}**님이 ${rejoined ? '재' : ''}가입하셨습니다!!\n\n🔥전체 유저 수: ${number2emoji(userCount)}명 돌파!!🔥`,
     );
   }
 
