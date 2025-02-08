86e0885bc628e7eafaf62c8596e8688a002fbba2
Im-Siyoun <rlatldbs3321@sju.ac.kr>
2025-02-08 20:49:04 +0900

    feat: admin api key에 암호화 체크 로직 추가

--- src/auth/guard/admin.guard.ts
+++ src/auth/guard/admin.guard.ts
@@ -1,4 +1,5 @@
 import {
+  BadRequestException,
   CanActivate,
   ExecutionContext,
   ForbiddenException,
@@ -6,16 +7,28 @@ import {
 } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { AdminApiStrategy } from './admin.strategy';
+import { PasswordService } from 'src/common/services/password.service';
 
 @Injectable()
 export class AdminApiGuard implements CanActivate {
-  constructor(private readonly adminApiStrategy: AdminApiStrategy) {}
+  constructor(
+    private readonly adminApiStrategy: AdminApiStrategy,
+    private readonly passwordService: PasswordService,
+  ) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
     const request = context.switchToHttp().getRequest();
 
-    const apiKey = request.headers['admin-api-key'];
+    const pattern =
+      /^[0-9a-fA-F]{32}:(?:[0-9a-fA-F]{32}|[0-9a-fA-F]{64}|[0-9a-fA-F]{96})$/;
+    if (!pattern.test(request.headers['admin-api-key'])) {
+      throw new BadRequestException('올바른 api key 형식이 아닙니다.');
+    }
+
+    const apiKey = this.passwordService.decryptPassword(
+      request.headers['admin-api-key'],
+    );
     const result = this.adminApiStrategy.validateApiKey(apiKey);
     if (!result) {
       throw new ForbiddenException('권한이 없습니다.');
--- src/notice/notice.module.ts
+++ src/notice/notice.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { NoticeController } from './notice.controller';
 import { NoticeRepository } from './notice.repository';
 import { NoticeService } from './notice.service';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
+  imports: [AuthModule],
   controllers: [NoticeController],
   providers: [NoticeService, NoticeRepository],
 })
