f8f61421e7ed926aa52dbc363bd119bb8fde054b
Im-Siyoun <rlatldbs3321@sju.ac.kr>
2025-06-05 10:47:20 +0900

    feat: 과제 단일조회 api 추가

--- src/assignment/assignment.service.ts
+++ src/assignment/assignment.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from "@nestjs/common";
 import { CreateUpdateAssignmentPayload } from "./payload/create-update-assignment.payload";
 import { UserInfo } from "src/auth/types/user-info.type";
 import { AssignmentRepository } from "./assignment.repository";
+import { Assignment } from "@prisma/client";
 
 @Injectable()
 export class AssignmentService {
@@ -9,15 +10,19 @@ export class AssignmentService {
     private readonly assignmentRepository: AssignmentRepository,
   ) {}
   
-  async createAssignment(user: UserInfo, payload: CreateUpdateAssignmentPayload) {
+  async createAssignment(user: UserInfo, payload: CreateUpdateAssignmentPayload): Promise<void> {
     return await this.assignmentRepository.createAssignment(user.studentId, payload);
   }
 
-  async updateAssignment(user: UserInfo, id: string, payload: CreateUpdateAssignmentPayload) {
+  async findAssignmentById(user: UserInfo, id: string): Promise<Assignment> {
+    return await this.assignmentRepository.findAssignmentById(user.studentId, id);
+  }
+
+  async updateAssignment(user: UserInfo, id: string, payload: CreateUpdateAssignmentPayload): Promise<void> {
     return await this.assignmentRepository.updateAssignment(user.studentId, id, payload);
   }
 
-  async deleteAssignment(user: UserInfo, id: string) {
+  async deleteAssignment(user: UserInfo, id: string): Promise<void> {
     return await this.assignmentRepository.deleteAssignment(user.studentId, id);
   }
 }
